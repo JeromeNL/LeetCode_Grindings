5be15a2bbc9d7563ad0e98ffb62b397f960eabf2
jeromenl <jw.kwetters@student.avans.nl>
2022-11-06 00:22:13 +0100

    Solved 1603. Design Parking System (with an array, after some browsing in discussion)

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,10 +1,10 @@
 import EasyProblems.LT_1470_ShuffleTheArray_Java;
+import EasyProblems.LT_1603_DesignParkingSystem_Java;
 import EasyProblems.LT_771_JewelsAndStones_Java;
 
 public class MainClass {
 
     public static void main(String[] args) {
-        LT_771_JewelsAndStones_Java test = new LT_771_JewelsAndStones_Java();
-        test.numJewelsInStones("z", "ZZ");
+
     }
 }
