9f5f729834357e84616cb1c2939ba2893b8e5411
jeromenl <jw.kwetters@student.avans.nl>
2022-11-17 23:55:05 +0100

    2469. Convert the Temperature O(1) - Most easy Leetcode problem ever.

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,14 +4,16 @@ import MediumProblems.LC_1476_SubrectangleQueries_Java;
 public class MainClass {
 
     public static void main(String[] args) {
-        int[][] testArray =
+
+
+       /* int[][] testArray =
                 {{3, 2, 1},
                  {2, 2, 2},
                  {1, 1, 1}};
 
         LC_1476_SubrectangleQueries_Java test = new LC_1476_SubrectangleQueries_Java(testArray);
 
-        System.out.print(test.getValue(2,1));
+        System.out.print(test.getValue(2,1));*/
 
 }
 }
