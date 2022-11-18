40a573b9c72bd0218083ffc0a705536607c7b447
jeromenl <jw.kwetters@student.avans.nl>
2022-11-18 23:14:30 +0100

    Solved 2418. Sort the People

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,19 +1,7 @@
-import EasyProblems.*;
-import MediumProblems.LC_1476_SubrectangleQueries_Java;
-
 public class MainClass {
 
     public static void main(String[] args) {
 
-
-       /* int[][] testArray =
-                {{3, 2, 1},
-                 {2, 2, 2},
-                 {1, 1, 1}};
-
-        LC_1476_SubrectangleQueries_Java test = new LC_1476_SubrectangleQueries_Java(testArray);
-
-        System.out.print(test.getValue(2,1));*/
 
 }
 }
