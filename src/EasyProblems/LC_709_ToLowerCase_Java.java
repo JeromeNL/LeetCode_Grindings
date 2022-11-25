74460ae0dfb73e9c815d979aae5938c86d1e9afa
jeromenl <jw.kwetters@student.avans.nl>
2022-11-25 01:21:03 +0100

    Solved 709. To Lower Case
    Solution with 3 words.

---
+++ src/EasyProblems/LC_709_ToLowerCase_Java.java
@@ -0,0 +1,7 @@
+package EasyProblems;
+
+public class LC_709_ToLowerCase_Java {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,11 @@
+import EasyProblems.LC_709_ToLowerCase_Java;
+
 public class MainClass {
 
     public static void main(String[] args) {
+        LC_709_ToLowerCase_Java test = new LC_709_ToLowerCase_Java();
 
+        System.out.print( test.toLowerCase("Hello"));
 
 }
 }
